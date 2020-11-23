0305145b9ac6b8170879267b8a9e6134cf593625
quarkusio <no-reply@quarkus.io>
2020-11-23 07:47:56 +0000

    Initial commit

---
+++ src/test/java/org/sunix/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.sunix;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
