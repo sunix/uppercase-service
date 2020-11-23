e28d57f4fb4feb036fdd02b7837bfdfd023f842f
sunix <sutan@redhat.com>
2020-11-23 10:24:38 +0000

    to uppercase
    Signed-off-by: sunix <sutan@redhat.com>

--- src/main/java/org/sunix/ExampleResource.java
+++ src/main/java/org/sunix/ExampleResource.java
@@ -2,15 +2,16 @@ package org.sunix;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/resteasy/hello")
+@Path("/touppercase")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String toUpperCase(@QueryParam("p") String p) {
+        return p.toUpperCase();
     }
 }
--- src/test/java/org/sunix/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.sunix;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
